2728ff8e3ed9fcef55b5c117698bbb0efd66de89
PaulinaArvizu <is707571@iteso.mx>
2021-10-20 22:53:03 -0500

    add messageCreate event for a Hello World

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 
 bot.once('ready', () => {
-    console.log('TUKI');
+    console.log('================================= TUKI BOT =================================');
     console.log(bot);
 });
 
+bot.on('messageCreate', msg => {
+    if (msg.content === 'tuki') {
+      return msg.reply('hello tuki');
+    }
+  });
 
 //This has to be last line of the script
 bot.login(process.env.BOT_TOKEN);
