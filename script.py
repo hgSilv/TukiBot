930b174c7405744fa6cb4c561c0ea7f8d7f247b9
Hector Silva <is712655@iteso.mx>
2021-12-01 20:54:16 -0600

    pipeline should create image and push it to ECR for each microservice

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import json;
 
 out = json.loads(open('running-tasks.json', 'r').read())
-command = 'aws ecs stop-task --cluster TukiCluster --task '+ str(out['taskArns'][out['taskArns'].index(n)])
-print(command)
+#command = 'aws ecs stop-task --cluster TukiCluster --task '+ str(out['taskArns'][out['taskArns'].index(n)])
+print(out['taskArns'][0])
