e5fa4807061d58b2386ab3719acf91a5b4272caa
Hector Silva <is712655@iteso.mx>
2021-12-02 01:06:49 -0600

    Pipeline should be now fully functional, deleted script.py, and task-def.json used by prev yaml file

--- script.py
+++
@@ -1,5 +0,0 @@
-import json;
-
-out = json.loads(open('running-tasks.json', 'r').read())
-#command = 'aws ecs stop-task --cluster TukiCluster --task '+ str(out['taskArns'][out['taskArns'].index(n)])
-print(out['taskArns'][0])
